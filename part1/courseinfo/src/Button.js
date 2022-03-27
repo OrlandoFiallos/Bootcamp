5330da42b983be64cd47b6430fdd61fe9892b719
Manuel Fiallos <contactofiallosorlando@gmail.com>
2022-03-27 16:34:02 -0600

    Estado del componente, controladores de eventos F

---
+++ part1/courseinfo/src/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Button = ({ handleClick, text }) => {
+  return (
+    <>
+      <button onClick={handleClick}>{text}</button>
+    </>
+  );
+};
--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,16 +1,26 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import { Hello } from "./Hello";
+import React, { useState } from "react";
+import { Display } from "./Display";
+import { Button } from "./Button";
 
 const App = () => {
-  const name = "Peter";
-  const age = 10;
+  const [contador, setContador] = useState(1);
 
+  const handleClick = () => setContador(contador + 1);
+  const reducirUno = () => setContador(contador - 1);
+  const setToZero = () => setContador(0);
+  const esPar = contador % 2 === 0;
+  const mensajePar = esPar ? "Es un número par." : "Es un número impar.";
   return (
     <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={10 + 10} />
-      <Hello name={name} age={age} />
+      <h1>Contador</h1>
+      <p>{mensajePar}</p>
+      <Display counter={contador} />
+      <Button handleClick={handleClick} text="Aumentar" />
+      <Button handleClick={reducirUno} text="Reducir en uno" />
+      <Button handleClick={setToZero} text="Resetear" />
+      <Hello name="Manuel" age="24" />
     </>
   );
 };
